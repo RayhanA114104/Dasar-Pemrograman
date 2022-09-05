17d1921cf9c39254bcb36a19c7296a4f661954cb
RayhanA114104 <112882829+RayhanA114104@users.noreply.github.com>
2022-09-05 22:49:42 +0700

    Add files via upload

---
+++ Pertemuan 1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nama: Rayhan Ulyabarran Priangga" << endl;
+    cout << "NIM: A11.2022.14131" << endl;
+    cout << "Alamat:Tegal,Jalan Baladewa 2 no.7" << endl;
+    cout << "Asal:Kota Tegal Jawa Tengah" << endl;
+
+
+}
