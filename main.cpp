7c6d498c831753af6b8b00044ed4cf45bbed09db
RayhanA114104 <112882829+RayhanA114104@users.noreply.github.com>
2022-09-12 15:30:58 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    /*printf ("rayhan");
+    */
+
+    int x = 20;
+    float t = 171.5;
+    char b = 'L';
+    string ub = "XXL";
+    cout << "umur saya: " << x;
+    cout << endl;
+    cout << "tinggi saya: " << t;
+    cout << "\nukuran baju: "<< b;
+
+
+
+    return 0;
+}
